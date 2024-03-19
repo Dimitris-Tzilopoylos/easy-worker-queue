c21850bf23896506d31e636a45000736ec99e0b8
dimtzilop <dimtzilop@iti.gr>
2024-03-19 20:05:21 +0200

    new version with new types

--- dist/lib/cron.d.ts
+++ dist/lib/cron.d.ts
@@ -1,7 +1,7 @@
 export = CronQueue;
 declare class CronQueue extends Queue {
-    schedule(namespace: any, scheduleString: any, options: any): void;
-    getTasks(): any;
-    getTask(namespace: any): any;
+  schedule(namespace: string, scheduleString: string, options?: any): void;
+  getTasks(): any;
+  getTask(namespace: any): any;
 }
 import Queue = require("./queue");
