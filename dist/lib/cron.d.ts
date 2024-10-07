fb48894550a2b54a01e8a191b504c58d92d39a96
dimtzilop <dimtzilop@iti.gr>
2024-10-07 10:16:09 +0300

    fixes types

--- dist/lib/cron.d.ts
+++ dist/lib/cron.d.ts
@@ -1,10 +1,6 @@
 export = CronQueue;
 declare class CronQueue extends Queue {
-  schedule(
-    namespace: string,
-    scheduleString: string,
-    options?: NodeCronOptions
-  ): void;
+  schedule(namespace: string, scheduleString: string, options?: any): void;
   getTasks(): any;
   getTask(namespace: string): any;
 }
