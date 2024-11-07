369f7ffb157fe05e2233a3d2fa70fc862838c79c
dimtzilop <dimtzilop@iti.gr>
2024-11-07 17:04:08 +0200

    returns task on schedule

--- dist/lib/cron.d.ts
+++ dist/lib/cron.d.ts
@@ -1,6 +1,6 @@
 export = CronQueue;
 declare class CronQueue extends Queue {
-  schedule(namespace: string, scheduleString: string, options?: any): void;
+  schedule(namespace: string, scheduleString: string, options?: any): any;
   getTasks(): any;
   getTask(namespace: string): any;
 }
